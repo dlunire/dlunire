59b11796ca1846cbe4d7f6c56e135378c0635afa
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2026-07-31 10:04:15 -0500

    Update

--- routes/welcome.php
+++ routes/welcome.php
@@ -1,15 +1,25 @@
 <?php
 
+/**
+ * Puede eliminar este archivo si lo desea. Borrarás automáticamente las rutas que
+ * vienen por defecto.
+ */
 use DLRoute\Requests\DLRoute;
 use DLUnire\Controllers\WelcomeController;
 
-DLRoute::get('/', [WelcomeController::class, 'index']);
+DLRoute::get("/", [WelcomeController::class, "index"]);
 
 # Privacidad
-DLRoute::get('/privacy', [WelcomeController::class, 'privacy']);
+DLRoute::get("/privacy", [WelcomeController::class, "privacy"]);
 
 # Términos y condiciones
-DLRoute::get('/terms', [WelcomeController::class, 'terms']);
+DLRoute::get("/terms", [WelcomeController::class, "terms"]);
 
 # Cookies
-DLRoute::get('/cookies', [WelcomeController::class, 'cookies']);
+DLRoute::get("/cookies", [WelcomeController::class, "cookies"]);
+
+# Script
+DLRoute::get("/script", [WelcomeController::class, "script"]);
+
+# Estilos
+DLRoute::get("/styles", [WelcomeController::class, "styles"]);
